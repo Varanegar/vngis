1917e8f645449012a22cca0380a12d28edc720ef
hooman.ahmadi@gmail.com <hooman.ahmadi@gmail.com>
2016-04-19 15:23:18 +0430

    SQL Bulk

---
+++ Source/R_and_D/V0/TrackingMap.Common/ViewModel/ComboFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingMap.Common.ViewModel
+{
+    public class ComboFilter
+    {
+        public string TblName { set; get; }
+        public string ValueName { set; get; }
+        public string TextName { set; get; }
+
+    }
+}
